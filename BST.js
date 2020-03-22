8d9a278bf2632ebde9f15bcbbc90ecd828d51142
abhilash0717 <40905548+abhilash0717@users.noreply.github.com>
2020-03-22 14:13:47 +0530

    Update BST.js

--- BST.js
+++ BST.js
@@ -60,6 +60,53 @@ class BinarySearchTree{
 
     return current;
   }
+
+  BFT(){
+      var data = [];
+      var queue = [];
+      var node = this.root;
+      queue.push(node);
+      while(queue.length){
+         node = queue.shift();
+         data.push(node.value);
+         if(node.left) queue.push(node.left);
+         if(node.right) queue.push(node.right);
+      }
+         return data;
+    }
+
+    DFSPreOrder(){
+        var data = [];
+        function traverse(node){
+            data.push(node.value);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right); 
+        }
+          traverse(this.root);
+        return data;
+    }
+
+    DFSPostOrder(){
+        var data = [];
+        function traverse(){
+        if(node.left) traverse(node.left);
+        if(node.right) traverse(node.right);
+        data.push(node.value);
+       }
+       traverse(this.root);
+        return data;
+    }
+
+    DFSInOrder(){
+         var data = [];
+        function traverse(){
+        if(node.left) traverse(node.left);
+         data.push(node.value);
+        if(node.right) traverse(node.right);
+       }
+       traverse(this.root);
+        return data;
+    }
  }
 
 var tree = new BinarySearchTree();
